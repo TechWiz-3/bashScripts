2ab997cab11033a3ba9ab6ba38b67468245fd41c
TechWiz-3 <programmingplus1@gmail.com>
2022-01-09 09:55:38 +1100

    tryna get things to work

--- read.sh
+++ read.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-echo -n "Type something and press enter: ";
-read dir
-echo You typed $dir
-permission="y"
-if (("$dir"="$permission")) ; then
-  echo "String is not empty"
+echo "Type something and press enter: ";
+a="yis"
+read ans
+if (("$a"=="yis")) ; then
+echo hallo;
+fi
+
+echo "Do that? [Y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "do that"
+else
+        echo "don't do that"
 fi
