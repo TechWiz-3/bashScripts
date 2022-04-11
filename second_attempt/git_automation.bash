c8f10c3fd5cb5d82d3ad5725b9d77af79be3fb30
TechWiz-3 <programmingplus1@gmail.com>
2022-04-11 17:02:47 +1000

    commit

--- second_attempt/git_automation.bash
+++ second_attempt/git_automation.bash
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-echo "Files: "
+echo "File: "
 read file
+file_path=$(/bin/pwd)
+echo "$file_path/$file"
+git add "$file_path/$file"
 echo "Commit message: "
 read commit_message
-git add $file
 git commit -m "$commit_message"
+git status
+sleep 3
+echo "Push changes? [y/n]"
+read ans
