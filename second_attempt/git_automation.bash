d305b5e28865fff58b880f3270747405f5305815
TechWiz-3 <programmingplus1@gmail.com>
2022-04-14 23:10:42 +1000

    more testing

--- second_attempt/git_automation.bash
+++ second_attempt/git_automation.bash
@@ -5,6 +5,41 @@ convey () {
     echo $2
 }  
 
+new_commit () {
+    echo $seperate
+    convey "Git status shown below" "GIT STATUS..." "Daniel"
+    sleep 1
+    git status
+    sleep 1
+    echo $seperate
+    sleep 2
+    convey "Enter the file path from current D I R... Also, X to escape the loop -r 10" "File: " "Fiona"
+    read file
+    while [ "$file" != "x" ]
+        do
+        file_path=$(/bin/pwd)
+        if [ "$file" == "all" ]
+            then
+            git add .
+        else
+            git add "$file_path/$file"
+            convey "Enter another file to add... Or leave with the lowercase letter x -r 8" "File: " "Daniel"
+            read file
+        fi
+    done
+    convey "Enter commit message -r 10" "Commit message: " "Alice"
+    read commit_message
+    echo "GIT COMMIT..."
+    git commit -m "$commit_message"
+    sleep 3
+    convey "Git status displayed below" $seperate "Moira"
+    sleep 1
+    echo "GIT STATUS..."
+    git status
+    echo $seperate
+    sleep 2
+    
+}
 seperate="------------------------------------------------------------------------"
 
 echo """
@@ -39,29 +74,36 @@ while [ "$file" != "x" ]
 done
 convey "Enter commit message -r 10" "Commit message: " "Alice"
 read commit_message
+echo "GIT COMMIT..."
 git commit -m "$commit_message"
 sleep 3
-convey "Git status displayed below" $seperate"\nGIT COMMIT" "Moira"
+convey "Git status displayed below" $seperate "Moira"
 sleep 1
-echo "GIT STATUS"
+echo "GIT STATUS..."
 git status
 echo $seperate
 sleep 2
-convey "Push changes... Y or N?" "Push changes? [y/n]" "Fiona"
-read push
-echo $seperate
-if [ "$push" == "y" ]
-    then
-    convey "Pushing changes" "PUSH CHANGES..." "Karen"
-    git push
-    sleep 4
-    echo $seperate
-    git status
+convey "Lowercase Y to create another commit... Lowercase N to proceed to pushing changes -r 8" "Create another commit? [y/n]" "Daniel"
+read answer
+if [ "$answer" == "y"]
+    do
+    new_commit
 else
-    convey "Git push cancelled -r 12" "Git push cancelled" "Karen"
-fi
-say -v Alex -r 230 "Program shutting down, thank you and see you next time"
-afplay /Users/Peregrine/Downloads/winterspell_30s.m4a
+    convey "Push changes... Y or N?" "Push changes? [y/n]" "Fiona"
+    read push
+    echo $seperate
+    if [ "$push" == "y" ]
+        then
+        convey "Pushing changes" "PUSH CHANGES..." "Karen"
+        git push
+        sleep 4
+        echo $seperate
+        git status
+    else
+        convey "Git push cancelled -r 12" "Git push cancelled" "Karen"
+    fi
+    say -v Alex -r 230 "Program shutting down, thank you and see you next time"
+    afplay /Users/Peregrine/Downloads/winterspell_30s.m4a
 
 
 #the real deal, pushing main
